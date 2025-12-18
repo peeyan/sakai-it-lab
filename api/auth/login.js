cf625244cf9e4a9bc96b301c5ec991ad80ff5caa
peeyan <miyaoka8290@outlook.jp>
2025-12-18 17:24:58 +0900

    jj

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,4 +1,4 @@
-import { getDbConnection } from '../_db';
+import { getDbConnection } from '../_db.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
