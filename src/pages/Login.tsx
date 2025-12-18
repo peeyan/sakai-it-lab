9de94338fe737cf0a2fb2e3231ec4d1cd6734b77
peeyan <miyaoka8290@outlook.jp>
2025-12-18 17:06:38 +0900

    改修

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ export const Login: React.FC = () => {
     setError('');
 
     try {
-      const res = await fetch('/api/auth/login', {
+      const res = await fetch('/api/auth/login.js', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
