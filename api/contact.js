bd8b562a395bd498a4ba28e0ec3ad21dd7c1c1d7
peeyan <miyaoka8290@outlook.jp>
2025-12-21 01:06:55 +0900

    dd

--- api/contact.js
+++ api/contact.js
@@ -1,10 +1,8 @@
 import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
-  console.log('★APIにアクセスがありました！');
-  console.log('メソッド:', req.method);
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: `Method not allowed ~ req.method:"${req.method}"` });
   }
 
   const { name, company, email, message } = req.body;
